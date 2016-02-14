6c3aa2bf44463af6d4b9547791fd1a73a0665186
Florian Letsch <florian@yootheme.com>
2016-02-14 17:12:12 +0100

    remember button presses and play the sequence every 4 presses.

--- piano/piano.ino
+++ piano/piano.ino
@@ -1,3 +1,6 @@
+#define SEQUENCE_MAX_LENGTH 4
+
+// Pin assignment
 int pinA = 2;
 int pinB = 4;
 int pinC = 6;
@@ -9,6 +12,13 @@ int pinLedC = 7;
 int pinLedD = 9;
 
 int speakerOut = 10;
+
+// used to code button sequences
+enum Button { BUTTON_A, BUTTON_B, BUTTON_C, BUTTON_D };
+
+// record the order of pressed buttons
+int sequence_input[SEQUENCE_MAX_LENGTH];
+int sequence_i = 0; // point to position which is to be filled next
 
 // Tones
 int  c = 3830;    // 261 Hz 
@@ -43,6 +53,7 @@ int aDown = 0;
 int bDown = 0;
 int cDown = 0;
 int dDown = 0;
+int lastButtonPressed = -1;
 
 void setup() {
   // put your setup code here, to run once:
@@ -61,25 +72,48 @@ void setup() {
   hello();
 }
 
+void playSequence(int sequence[], int count)
+{
+  
+  long d = 150000;
+  int _tone;
+  int led;
+  
+  for (int i = 0; i<count; i++) {
+    
+    switch (sequence[i])
+    {
+      case BUTTON_A: 
+        _tone = toneA;
+        led = pinLedA;
+      break;
+      case BUTTON_B: 
+        _tone = toneB;
+        led = pinLedB;
+      break;
+      case BUTTON_C: 
+        _tone = toneC;
+        led = pinLedC;
+      break;
+      case BUTTON_D: 
+        _tone = toneD;
+        led = pinLedD;
+      break;
+    }
+
+    // light and sound
+    digitalWrite(led, HIGH);
+    playTone(_tone, d);
+    digitalWrite(led, LOW);
+  }
+}
+
 void hello()
 {
-  long d = 200000;
 
-  digitalWrite(pinLedA, HIGH);
-  playTone(toneA, d);
-  digitalWrite(pinLedA, LOW);
+  int sequence[] = { BUTTON_A, BUTTON_B, BUTTON_C, BUTTON_D };
 
-  digitalWrite(pinLedB, HIGH);
-  playTone(toneB, d);
-  digitalWrite(pinLedB, LOW);
-
-  digitalWrite(pinLedC, HIGH);
-  playTone(toneC, d);
-  digitalWrite(pinLedC, LOW);
-
-  digitalWrite(pinLedD, HIGH);
-  playTone(toneD, d);
-  digitalWrite(pinLedD, LOW);
+  playSequence(sequence, 4);
 
   delay(100);
 
@@ -115,34 +149,76 @@ void playTone(int tone_, long duration) {
   }           
 }
 
-void loop() {
-
-  // turn LEDs off by default
-  digitalWrite(pinLedA, LOW);
-  digitalWrite(pinLedB, LOW);
-  digitalWrite(pinLedC, LOW);
-  digitalWrite(pinLedD, LOW);
-
-  // read button states
+void readButtons() 
+{
   aDown = digitalRead(pinA);
   bDown = digitalRead(pinB);
   cDown = digitalRead(pinC);
   dDown = digitalRead(pinD);
+}
+
+void ledsOff() 
+{
+  digitalWrite(pinLedA, LOW);
+  digitalWrite(pinLedB, LOW);
+  digitalWrite(pinLedC, LOW);
+  digitalWrite(pinLedD, LOW);
+}
+
+void loop() {
+
+  // turn LEDs off by default
+  ledsOff();
+  
+  // read button states
+  readButtons();
+
+  // assume button is still pressed. FIXME: this doesnt allow for same note twice
+  int buttonPressed = lastButtonPressed;
 
   if (aDown) {
     digitalWrite(pinLedA, HIGH);
     playTone(toneA, toneA*toneDurationMultiplier);
+    buttonPressed = BUTTON_A;
   }
   if (bDown) {
     digitalWrite(pinLedB, HIGH);
     playTone(toneB, toneB*toneDurationMultiplier);
+    buttonPressed = BUTTON_B;
   }
   if (cDown) {
     digitalWrite(pinLedC, HIGH);
     playTone(toneC, toneC*toneDurationMultiplier);
+    buttonPressed = BUTTON_C;
   }
   if (dDown) {
     digitalWrite(pinLedD, HIGH);
     playTone(toneD, toneD*toneDurationMultiplier);
+    buttonPressed = BUTTON_D;
   }
+
+  if (buttonPressed != lastButtonPressed) {
+    // remember this button  
+    sequence_input[sequence_i++] = buttonPressed;
+    lastButtonPressed = buttonPressed;
+
+    if (sequence_i == SEQUENCE_MAX_LENGTH) {
+
+      // wait for buttons to be released
+      while (aDown || bDown || cDown || dDown) {
+        readButtons();
+      }
+
+      ledsOff();
+    
+      delay(100);
+      
+      // play sequence
+      playSequence(sequence_input, SEQUENCE_MAX_LENGTH);
+      
+      // reset sequence collector
+      sequence_i = 0;
+    }
+  }
+  
 }
