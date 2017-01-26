6558714507173a6a68e86b875e83bf522f47b89b
Florian Letsch <florian@yootheme.com>
2017-01-27 00:26:55 +0100

    button press is registered at any time, not just between executions of patterns

--- cyberalarm/cyberalarm.ino
+++ cyberalarm/cyberalarm.ino
@@ -15,78 +15,74 @@ int longDelay = 500;
 int pattern = 0;
 int countPattern = 4;
 
+// state variables
+unsigned long timer = 0;
 int lastState = LOW;
 
-void ledOn(int ledNumber) {
-  digitalWrite(ledNumber, HIGH);
-}
-
-void ledOff(int ledNumber) {
-  digitalWrite(ledNumber, LOW);
+void setLEDs(byte pattern) {
+  digitalWrite(pinLedA, HIGH && (pattern & B0001));
+  digitalWrite(pinLedB, HIGH && (pattern & B0010));
+  digitalWrite(pinLedC, HIGH && (pattern & B0100));
+  digitalWrite(pinLedD, HIGH && (pattern & B1000));
 }
 
 
 // all one, all off
-void pattern0() {
-  ledsOn();
-  delay(longDelay);
-  ledsOff();
-  delay(longDelay);
+byte pattern0(long timer) {
+  int thisDuration = 1000;
+  if (timer % thisDuration < 500) {
+    return B1111; // all ON
+  } else {
+    return B0000; // all OFF
+  }
 }
 
 // blink a cross
-void pattern1() {
-  ledOn(pinLedA);
-  ledOn(pinLedC);
-  delay(longDelay);
-  ledsOff();
-
-  ledOn(pinLedB);
-  ledOn(pinLedD);
-  delay(longDelay);
-  ledsOff();
+byte pattern1(long timer) {
+  int thisDuration = 1000;
+  if (timer % thisDuration < 500) {
+    return B0101;
+  } else {
+    return B1010;
+  }
 }
 
 // just round, one by one
-void pattern2() {
-  ledOn(pinLedA);
-  delay(shortDelay);
-  ledsOff();
-  ledOn(pinLedB);
-  delay(shortDelay);
-  ledsOff();
-  ledOn(pinLedC);
-  delay(shortDelay);
-  ledsOff();
-  ledOn(pinLedD);
-  delay(shortDelay);
-  ledsOff();
+byte pattern2(long timer) {
+  int thisDuration = 2000;
+  int thisRun = timer % thisDuration;
+  if (thisRun < 500) {
+    return B0001;
+  } else if(thisRun < 1000) {
+    return B0010;
+  } else if(thisRun < 1500) {
+    return B0100;
+  } else {
+    return B1000;
+  }
 }
 
 // round: all on, one by one. then round: all off, one by one.
-void pattern3() {
-  
-  // all on, one by one
-  ledOn(pinLedA);
-  delay(shortDelay);
-  ledOn(pinLedB);
-  delay(shortDelay);
-  ledOn(pinLedC);
-  delay(shortDelay);
-  ledOn(pinLedD);
-  
-  delay(longDelay);
-
-  // all off, one by one
-  ledOff(pinLedA);
-  delay(shortDelay);
-  ledOff(pinLedB);
-  delay(shortDelay);
-  ledOff(pinLedC);
-  delay(shortDelay);
-  ledOff(pinLedD);
-  
-  delay(longDelay); 
+byte pattern3(long timer) {
+  int thisDuration = 4000;
+  int thisRun = timer % thisDuration;
+  if (thisRun < 500) {
+    return B0000;
+  } else if(thisRun < 1000) {
+    return B0001;
+  } else if(thisRun < 1500) {
+    return B0011;
+  } else if(thisRun < 2000) {
+    return B0111;
+  } else if(thisRun < 2500) {
+    return B1111;
+  } else if(thisRun < 3000) {
+    return B1110;
+  } else if(thisRun < 3500) {
+    return B1100;
+  } else {
+    return B1000;
+  }
 }
 
 void setup() {
@@ -96,37 +92,28 @@ void setup() {
   pinMode(pinLedD, OUTPUT);
 }
 
-void ledsOff() 
-{
-  digitalWrite(pinLedA, LOW);
-  digitalWrite(pinLedB, LOW);
-  digitalWrite(pinLedC, LOW);
-  digitalWrite(pinLedD, LOW);
-}
-
-void ledsOn() 
-{
-  digitalWrite(pinLedA, HIGH);
-  digitalWrite(pinLedB, HIGH);
-  digitalWrite(pinLedC, HIGH);
-  digitalWrite(pinLedD, HIGH);
-}
-
 void loop() {
+
+  timer = millis();
+  
   // should move to next pattern?
   int newState = digitalRead(pinButton);
   if ((newState != lastState)) { // button pressed?
     if (newState == HIGH) {
       pattern = (pattern+1) % countPattern; // next pattern!  
+      delay(100);
     }
     lastState = newState;
   }
 
   // play ze pattern
+  byte bits;
   switch(pattern) {
-    case 0: pattern0(); break;
-    case 1: pattern1(); break;
-    case 2: pattern2(); break;
-    case 3: pattern3(); break;
+    case 0: bits = pattern0(timer); break;
+    case 1: bits = pattern1(timer); break;
+    case 2: bits = pattern2(timer); break;
+    case 3: bits = pattern3(timer); break;
   }
+
+  setLEDs(bits);
 }
